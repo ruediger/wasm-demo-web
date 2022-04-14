4d2ae6bc98f7e57424060d619114a6db958bd581
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2022-04-14 11:56:11 +0100

    Commit running version.
    When I picked this up again it wasn't compiling. Now it is. But chat
    isn't working.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,118 @@
+use futures::{StreamExt, FutureExt};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::{mpsc, RwLock};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use uuid::Uuid;
 use warp::Filter;
+
+struct Client {
+    name: String,
+    sender: Option<mpsc::UnboundedSender<Result<warp::ws::Message, warp::Error>>>,
+}
+
+impl Client {
+    fn new(name: String) -> Self {
+        Client {
+            name: name,
+            sender: None,
+        }
+    }
+}
+
+type Clients = Arc<RwLock<HashMap<Uuid, Client>>>;
+
+#[derive(Deserialize, Debug)]
+struct RegisterRequest {
+    name: String,
+}
+
+#[derive(Serialize, Debug)]
+struct RegisterResponse {
+    uuid: Uuid,
+}
+
+async fn register_client(rreq: RegisterRequest, clients: Clients) -> std::result::Result<impl warp::Reply, warp::reject::Rejection>  {
+    println!("register_client!");
+    let uuid = Uuid::new_v4();
+    clients.write().await.insert(uuid, Client::new(rreq.name));
+    Ok(warp::reply::json(&RegisterResponse { uuid }))
+}
+
+async fn broadcast_message(id: &Uuid, msg: warp::ws::Message, clients: &Clients) {
+    let msg_txt = match msg.to_str() {
+        Ok(m) => m,
+        Err(_) => return,
+    };
+
+    let client = clients.read().await;
+    let client = client.get(id);
+    if client.is_none() {
+        return;
+    }
+    let client = client.unwrap();
+    let msg_txt = format!("<{}> {}", client.name, msg_txt);
+
+    for (&uid, client) in clients.read().await.iter() {
+        if uid != *id {
+            if let Some(sender) = &client.sender {
+                if let Err(_) = sender.send(Ok(warp::ws::Message::text(msg_txt.clone()))) {
+                    // disconnect should be handled in client_connected.  Just ignore here.
+                }
+            }
+        }
+    }
+}
+
+async fn client_connected(socket: warp::ws::WebSocket, id: Uuid, clients: Clients) {
+    let (ws_tx, mut ws_rx) = socket.split();
+
+    let (tx, rx) = mpsc::unbounded_channel();
+
+    {
+        let mut client = clients.write().await;
+        let client = client.get_mut(&id);
+        if client.is_none() {
+            return;
+        }
+        let mut client = client.unwrap();
+        client.sender = Some(tx);
+    }
+
+    let rx = UnboundedReceiverStream::new(rx);
+    tokio::task::spawn(rx.forward(ws_tx).map(|result| {
+        if let Err(e) = result {
+            eprintln!("websocket send error: {}", e);
+        }
+    }));
+
+    // Every time the user sends a message, broadcast it to
+    // all other users...
+    while let Some(result) = ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("websocket error(uid={}): {}", id, e);
+                break;
+            }
+        };
+        broadcast_message(&id, msg, &clients).await;
+    }
+
+    client_disconnected(&id, &clients).await;
+}
+
+async fn client_disconnected(id: &Uuid, clients: &Clients) {
+    eprintln!("Disconnected: {}", id);
+
+    clients.write().await.remove(id);
+}
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = std::convert::Infallible> + Clone {
+    println!("with_clients!");
+    warp::any().map(move || clients.clone())
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,7 +127,24 @@ async fn main() {
     let webdata_files = warp::get().and(warp::path("webdata")).and(warp::fs::dir("../../webdata/"));
     let pkg_files = warp::get().and(warp::path("pkg")).and(warp::fs::dir("../../pkg/"));
 
-    let routes = hello.or(webdata_files.or(pkg_files.or(index)));
+    let clients = Clients::default();
+    let registration = warp::post()
+        .and(warp::path("register"))
+        .and(warp::body::json())  // TODO: add content_length_limit
+        .and(with_clients(clients.clone()))
+        .and_then(register_client);
+    let chat = warp::get()
+        .and(warp::path("chat"))
+        .and(warp::ws())  // WebSocket Handshake
+        .and(warp::path::param())
+        .and(with_clients(clients.clone()))
+        .map(|ws: warp::ws::Ws, id, clients| {
+            ws.on_upgrade(move |socket| client_connected(socket, id, clients))
+        });
+
+    let routes = hello.or(webdata_files.or(pkg_files.or(index.or(registration.or(chat)))));
+
+    println!("Starting server: http://127.0.0.1:3030/");
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
+use futures::TryFutureExt;
+use js_sys::Promise;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::console;
+use web_sys::{Headers, Request, RequestInit, RequestMode, Response};
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
     let window = web_sys::window().expect("no global `window` exists");
@@ -55,7 +62,7 @@ pub fn main() -> Result<(), JsValue> {
     canvas.set_onmousedown(Some(mousedown.as_ref().unchecked_ref()));
     mousedown.forget();
 
-    Ok(())
+    chat_init()
 }
 
 #[wasm_bindgen]
@@ -70,4 +77,118 @@ pub fn scroll_test() {
 #[wasm_bindgen]
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
+}
+
+////////////////////////////////////////////////////////////
+
+// TODO: have structs in common lib for server+client!
+#[derive(Serialize, Debug)]
+struct RegisterRequest {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct RegisterResponse {
+    uuid: String,  // Uuid
+}
+
+#[wasm_bindgen]
+pub fn chat_init() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    // Connect actions
+    let registration_div = document.get_element_by_id("registration").expect(
+        "should have `registration` div element");
+    let registration_div: web_sys::HtmlDivElement = registration_div
+        .dyn_into::<web_sys::HtmlDivElement>()
+        .expect("div element `registration`");
+
+    let name_input = document.get_element_by_id("name").expect("should have `name` input element");
+    let name_input: web_sys::HtmlInputElement = name_input
+        .dyn_into::<web_sys::HtmlInputElement>()
+        .expect("input element `name`");
+
+    let connect_input = document.get_element_by_id("connect").expect("should have `connect` input element");
+    let connect_input: web_sys::HtmlInputElement = connect_input
+        .dyn_into::<web_sys::HtmlInputElement>()
+        .expect("input element `connect`");
+
+    let live_div = document.get_element_by_id("live").expect("should have `live` div element");
+    let live_div: web_sys::HtmlDivElement = live_div
+        .dyn_into::<web_sys::HtmlDivElement>()
+        .expect("div element `live`");
+
+    let chatmessages_area = document.get_element_by_id("chatmessages").expect(
+        "should have `chatmessages` textarea element");
+    let chatmessages_area: web_sys::HtmlTextAreaElement = chatmessages_area
+        .dyn_into::<web_sys::HtmlTextAreaElement>()
+        .expect("textarea element `chatmessages`");
+
+    let message_input = document.get_element_by_id("message").expect("should have `message` input element");
+    let message_input: web_sys::HtmlInputElement = message_input
+        .dyn_into::<web_sys::HtmlInputElement>()
+        .expect("input element `message`");
+
+    let send_button = document.get_element_by_id("send").expect("should have `send` input element");
+    let send_button: web_sys::HtmlInputElement = send_button
+        .dyn_into::<web_sys::HtmlInputElement>()
+        .expect("input element `send`");
+
+    let connect_action = Closure::wrap(Box::new(move || {
+        console::log_2(&"Action action action: ".into(), &name_input.value().into());
+
+        if name_input.value() == "" {
+            // TODO: validate name (also on server)
+            return;
+        }
+
+        let fut = register_chat(name_input.value())
+            .map_ok(|uuid: String| {
+                console::log_2(&"UUID: ".into(), &JsValue::from(uuid));
+            });
+        wasm_bindgen_futures::spawn_local(async { fut.await; });
+
+        live_div.style().set_property("display", "block").expect("display block of live div failed");
+        registration_div.style().set_property("display", "none").expect("display none of registration div failed");
+    }) as Box<dyn Fn()>);
+    connect_input.set_onclick(Some(connect_action.as_ref().unchecked_ref()));
+    connect_action.forget();
+
+    Ok(())
+}
+
+pub fn register_chat(name: String) -> impl futures::Future<Output = std::result::Result<String, JsValue>> {
+    // TODO: too many expect. Better error handling!
+
+    let mut opts = RequestInit::new();
+    opts.method("POST");
+    opts.mode(RequestMode::Cors);
+
+    let register_request = RegisterRequest {
+        name: name,
+    };
+    // let register_request = JsValue::from_serde(&register_request).expect(
+    //     "failed conversion from RegisterRequest to JSON");
+    // opts.body(Some(&register_request));
+    let register_request = serde_json::to_string(&register_request).expect("failed conversion from RegisterRequest to JSON");
+    let register_request = JsValue::from(register_request);
+    opts.body(Some(&register_request));
+
+    let request = Request::new_with_str_and_init(&"/register", &opts).expect(
+        "request to be created");
+    request.headers().set("Content-Type", "application/json; charset=utf-8").expect("content-type");
+
+    let window = web_sys::window().expect("no global `window` exists");
+    JsFuture::from(window.fetch_with_request(&request)).map_ok(|resp_value| {
+        // `resp_value` is a `Response` object.
+        let resp: Response = resp_value.dyn_into().expect("resp_value should be a Response object");
+        resp.json().expect("response to be json")
+    }).and_then(|json_value: Promise| {
+        JsFuture::from(json_value)
+    }).map_ok(|json| {
+        let register_response : RegisterResponse = json.into_serde().expect(
+            "expected RegisterResponse response type");
+        register_response.uuid
+    })
 }
